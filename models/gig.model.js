be04513d9a086cda632e72c18fb885625bc91423
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-03 19:08:07 +0200

    created gig model

---
+++ models/gig.model.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const GigSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    totalRating: {
+      type: Number,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    categoryId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    coverImg: {
+      type: String,
+      required: true,
+    },
+    imgs: {
+      type: [String],
+      required: false,
+    },
+    shortTitle: {
+      type: String,
+      required: true,
+    },
+    shortDescription: {
+      type: String,
+      required: true,
+    },
+    deliveryTime: {
+      type: Number,
+      required: true,
+    },
+    revisionNumber: {
+      type: Number,
+      required: true,
+    },
+    features: {
+      type: [String],
+      required: false,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Gig", GigSchema);
