9aa2f71d37d5121b795d52b8542f453a2b214f6b
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-03 20:16:33 +0200

    added index file for easing the imports

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Conversation = require("./conversation.model");
+const Gig = require("./gig.model");
+const Message = require("./message.model");
+const Order = require("./order.model");
+const Review = require("./review.model");
+const User = require("./user.model");
+
+module.exports = {
+  Conversation,
+  Gig,
+  Message,
+  Order,
+  Review,
+  User,
+};
