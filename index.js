0eb71b706663ac0ca8daefb68c112de921dca353
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-04 02:47:49 +0200

    setup routese , cors , db connection

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+const {
+  userRoutes,
+  conversationRoutes,
+  gigRoutes,
+  messageRoutes,
+  orderRoutes,
+  reviewRoutes,
+  authRoutes,
+} = require("./routes");
 
 const databaseConnection = async () => {
   try {
@@ -11,6 +22,18 @@ const databaseConnection = async () => {
     console.log(error);
   }
 };
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/users", userRoutes);
+app.use("/api/conversations", conversationRoutes);
+app.use("/api/gigs", gigRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(4000, async () => {
   await databaseConnection();
