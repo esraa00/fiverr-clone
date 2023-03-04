b4a7d985be96887baf36b5cf8af6ef42f45e0720
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-04 20:15:03 +0200

    created controller for deleting gig

--- controller/gig.controller.js
+++ controller/gig.controller.js
@@ -11,7 +11,23 @@ const createGig = async (req, res) => {
     res.send(error);
   }
 };
+
+const deleteGig = async (req, res) => {
+  try {
+    const gigToBeDeleted = await Gig.findById(req.params.id);
+    if (!gigToBeDeleted)
+      return res.status(404).send("this resource is already deleted");
+    if (gigToBeDeleted.userId !== req.userId)
+      return res
+        .status(403)
+        .send("you don't have permission to perform this action");
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).send("deleted successfully");
+  } catch (error) {
+    res.send(error);
+  }
+};
+
 const getAllGigs = async (req, res) => {};
-const deleteGig = async (req, res) => {};
 const getGig = async (req, res) => {};
 module.exports = { createGig, getAllGigs, deleteGig, getGig };
