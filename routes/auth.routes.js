1b166ad50ee3e10315bcc048f23d99d24df6f604
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-04 03:25:09 +0200

    added route for logging out

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { authController } = require("../controller");
+const { isAuthenticated } = require("../middleware");
 
 router.route("/register").post(authController.register);
 router.route("/login").post(authController.login);
-router.route("/logout").post(authController.logout);
+router.route("/logout").post(isAuthenticated, authController.logout);
 
 module.exports = router;
