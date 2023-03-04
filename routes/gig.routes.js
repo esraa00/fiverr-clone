37b4d780c75c5b262f200e32e0f06cd6e212f47a
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-04 19:03:17 +0200

    setup routes

--- routes/gig.routes.js
+++ routes/gig.routes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
+const { isAuthenticated, isSeller } = require("../middleware");
 const { gigController } = require("../controller");
+
+router
+  .route("/")
+  .post(isAuthenticated, isSeller, gigController.createGig)
+  .get(gigController.getAllGigs);
+router
+  .route("/:id")
+  .delete(isAuthenticated, gigController.deleteGig)
+  .get(gigController.getGig);
 
 module.exports = router;
